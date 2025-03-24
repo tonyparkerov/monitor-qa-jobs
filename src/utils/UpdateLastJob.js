f222c6481e501aa6ede804ad0ac1b8438cb996b5
tonyparkerov <antonproskurov@gmail.com>
2025-03-24 13:17:05 +0200

    test

---
+++ src/utils/UpdateLastJob.js
@@ -0,0 +1,12 @@
+import fs from "fs";
+const path = "./src/config/last_job.txt";
+
+let lastJob = "No previous job recorded";
+if (fs.existsSync(path)) {
+  lastJob = fs.readFileSync(path, "utf8").trim();
+}
+
+const newLastJob = "new Date().toISOString()";
+fs.writeFileSync(path, newLastJob);
+
+export { lastJob, newLastJob };
